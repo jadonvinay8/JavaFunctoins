f7c31bd0670b460e548b2a91f1cbea0487cbb0d3
Vinay Pratap Singh <jadonvinay8@gmail.com>
2020-12-16 17:49:06 +0530

    CI/CD check

--- src/main/java/org/example/functions/HttpTriggerFunction.java
+++ src/main/java/org/example/functions/HttpTriggerFunction.java
@@ -42,7 +42,7 @@ public class HttpTriggerFunction {
         } else {
             return request
               .createResponseBuilder(HttpStatus.OK)
-              .body("Hello, " + name + ". It's nice to meet you")
+              .body("Hello " + name + ". It's nice to meet you\n CI/CD is working")
               .build();
         }
     }
